5a5f5be3806c3c1b006c9312ec426c440d6e7c7d
Enrique Sapien <esapien@hotmail.com>
2018-11-11 17:45:20 -0600

    Add command line arguments retrieval

--- app.js
+++ app.js
@@ -7,5 +7,19 @@ const _ = require('lodash');
 // Local modules
 const notes = require('./notes');
 
+var command = process.argv[2];
 
+// print the array or arguments passed from command line
+console.log('Command: ', command);
 
+if (command === 'add') {
+    console.log('Adding new note');
+} else if (command === 'list') {
+    console.log('Listing all notes');
+} else if (command === 'read') {
+    console.log('Reading a note');
+} else if (command === 'remove') {
+    console.log('Removing a note');
+} else {
+    console.log('Command not recognized');
+}

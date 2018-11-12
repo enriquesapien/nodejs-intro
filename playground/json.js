37c88151be15156e5d1ffabc82ccdcc6e765141c
Enrique Sapien <esapien@hotmail.com>
2018-11-11 19:08:26 -0600

    Add sample basic JSON/JS object management

--- playground/json.js
+++ playground/json.js
@@ -1,8 +1,8 @@
-// var obj = {
-//     name: 'Enrique'
-// };
+var obj = {
+    name: 'Enrique'
+};
 
-// convert object into JSON string
+// convert JavaScript object into JSON string
 // var stringObj = JSON.stringify(obj);
 
 // console.log(typeof obj);
@@ -11,10 +11,29 @@
 // console.log(stringObj);
 
 
-var personString = '{"id" : 1, "name" : "Enrique"}';
+// var personString = '{"id" : 1, "name" : "Enrique"}';
 
-// convert JSON string into object
-var person = JSON.parse(personString);
+// // convert JSON string into JavaScript object
+// var person = JSON.parse(personString);
 
-console.log(typeof person);
-console.log(person);
+// console.log(typeof person);
+// console.log(person);
+
+const fs = require('fs');
+
+// sample JS object
+var originalNote = {
+    title: 'Some title',
+    body: 'Some body'
+};
+
+// Stringify JS object to save into JSON text file
+var originalNoteString = JSON.stringify(originalNote);
+fs.writeFileSync('notes.json', originalNoteString);
+
+// retrieve string from text file and convert to JS object
+var noteString = fs.readFileSync('notes.json');
+var noteObj = JSON.parse(noteString);
+
+console.log(typeof noteObj);
+console.log(noteObj);

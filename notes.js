d6de9857a742c4ee3c9c4b8f85206f895846d427
Enrique Sapien <esapien@hotmail.com>
2018-11-11 18:16:28 -0600

    Add 'list' functionality

--- notes.js
+++ notes.js
@@ -4,6 +4,11 @@ var addNote = (title, body) => {
     console.log('Adding note', title, body);
 };
 
+var getAll = () => {
+    console.log('Getting all notes');
+};
+
 module.exports = {
-    addNote // if equal, same as: addNote: addNote in ES6
+    addNote, // if equal, same as: addNote: addNote in ES6
+    getAll
 }
